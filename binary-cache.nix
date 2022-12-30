1152232443ad6681217aea47b64ae0749dfc5b6a
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-30 04:20:32 +0000

    Make sysupgrade more useful

--- binary-cache.nix
+++ binary-cache.nix
@@ -10,14 +10,20 @@
 
   environment.systemPackages = [( pkgs.writeShellScriptBin "sysupgrade" ''
     if [ -z "$1" ]; then
+      echo "Usage: sysupgrade (switch/boot/...] [new-hostname]"
+      exit 1
+    fi
+    if [ -z "$2" ]; then
       hostname="${config.networking.hostName}"
     else
-      hostname=$1
+      hostname=$2
+      echo "New hostname: $hostname"
+      echo "If this was a mistake, cancel and revert by running 'sysupgrade switch ${config.networking.hostName}'"
     fi
     system_path=$(${pkgs.curl}/bin/curl --location --header "Accept: application/json" \
       "http://hydra.v6.fyi/job/nixos-systems/main/$hostname.${config.nixpkgs.system}/latest-finished" | ${pkgs.jq}/bin/jq -r '.buildoutputs.out.path')
     echo "Next generation: $system_path"
     ${config.nix.package}/bin/nix-env --profile /nix/var/nix/profiles/system --set "$system_path"
-    /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+    /nix/var/nix/profiles/system/bin/switch-to-configuration $1
   '')];
 }
