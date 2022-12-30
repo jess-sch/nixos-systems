a2c2465a3d3a7f1eafa1989d88949f7f2cbd125b
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-30 04:12:25 +0000

    Add hostname switching functionality

--- binary-cache.nix
+++ binary-cache.nix
@@ -9,8 +9,13 @@
   ];
 
   environment.systemPackages = [( pkgs.writeShellScriptBin "sysupgrade" ''
+    if [ -z "$1" ]; then
+      hostname="${config.networking.hostName}"
+    else
+      hostname=$1
+    fi
     system_path=$(${pkgs.curl}/bin/curl --location --header "Accept: application/json" \
-      "http://hydra.v6.fyi/job/nixos-systems/main/${config.networking.hostName}.${config.nixpkgs.system}/latest-finished" | ${pkgs.jq}/bin/jq -r '.buildoutputs.out.path')
+      "http://hydra.v6.fyi/job/nixos-systems/main/$hostname.${config.nixpkgs.system}/latest-finished" | ${pkgs.jq}/bin/jq -r '.buildoutputs.out.path')
     echo "Next generation: $system_path"
     ${config.nix.package}/bin/nix-env --profile /nix/var/nix/profiles/system --set "$system_path"
     /nix/var/nix/profiles/system/bin/switch-to-configuration switch
