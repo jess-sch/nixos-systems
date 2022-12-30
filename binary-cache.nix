058bcf0aec84b6cd16558e53b61e408c24f132b1
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-30 03:48:54 +0000

    Fix jq in sysupgrade

--- binary-cache.nix
+++ binary-cache.nix
@@ -9,10 +9,8 @@
   ];
 
   environment.systemPackages = [( pkgs.writeShellScriptBin "sysupgrade" ''
-    alias curl="${pkgs.curl}/bin/curl"
-    alias jq="${pkgs.jq}/bin/jq"
-    system_path=$(curl --location --header "Accept: application/json" \
-      "http://hydra.v6.fyi/job/nixos-systems/main/${config.networking.hostName}.${config.nixpkgs.system}/latest-finished" | jq -r '.buildoutputs.out.path')
+    system_path=$(${pkgs.curl}/bin/curl --location --header "Accept: application/json" \
+      "http://hydra.v6.fyi/job/nixos-systems/main/${config.networking.hostName}.${config.nixpkgs.system}/latest-finished" | ${pkgs.jq}/bin/jq -r '.buildoutputs.out.path')
     ${config.nix.package}/bin/nix-env --profile /nix/var/nix/profiles/system --set "$system_path"
     /nix/var/nix/profiles/system/bin/switch-to-configuration switch
   '')];
