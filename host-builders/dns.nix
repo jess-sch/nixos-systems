0148587479d14decf7f92d2c39deb281d1c75204
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-30 11:00:51 +0000

    fix unbound dns64 config

--- host-builders/dns.nix
+++ host-builders/dns.nix
@@ -24,8 +24,8 @@ in
       name = ".";
       forward-addr = "2a06:98c1:54::3:b4c6";
     }];
-    settings.module-config = (if dns64 then "dns64 " else "") + "validator iterator";
+    settings.server.module-config = (if dns64 then "dns64 " else "") + "validator iterator";
   };
 } // (if dns64 then {
-  services.unbound.settings.dns64-prefix = "64:ff9b::/96";
+  services.unbound.settings.server.dns64-prefix = "64:ff9b::/96";
 } else { })
