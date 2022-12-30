49f94d6a08c627bb9e7e29a583f79f4151f4c658
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-30 20:00:01 +0000

    Extract sysupgrade to separate file

---
+++ shared-modules/sysupgrade.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }:
+let
+  sysupgradePkg = pkgs.writeShellScriptBin "sysupgrade" ''
+    if [ -z "$1" ]; then
+      echo "Usage: sysupgrade (switch/boot/...) [new-hostname]"
+      exit 1
+    fi
+    if [ -z "$2" ]; then
+      hostname="${config.networking.hostName}"
+    else
+      hostname=$2
+      echo "New hostname: $hostname"
+      echo "If this was a mistake, cancel and revert by running 'sysupgrade switch ${config.networking.hostName}'"
+    fi
+    system_path=$(${pkgs.curl}/bin/curl --location --header "Accept: application/json" \
+      "http://hydra.v6.fyi/job/nixos-systems/main/$hostname.${config.nixpkgs.system}/latest-finished" | ${pkgs.jq}/bin/jq -r '.buildoutputs.out.path')
+    if [ "$system_path" = "null" ]; then
+      echo "Error: There is no configuration for $hostname"
+      exit 1
+    fi
+    echo "Next generation: $system_path"
+    ${config.nix.package}/bin/nix-env --profile /nix/var/nix/profiles/system --set "$system_path"
+    /nix/var/nix/profiles/system/bin/switch-to-configuration $1
+  '';
+in
+{
+  environment.systemPackages = lib.mkAfter [ sysupgradePkg ];
+}
