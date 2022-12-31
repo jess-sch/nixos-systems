926daa4e7a94d3c8cbc10c2f39cb93b02beefa58
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-31 04:33:03 +0000

    Remove hydra-cache domain from substiituters

--- shared-modules/binary-cache.nix
+++ shared-modules/binary-cache.nix
@@ -2,7 +2,6 @@
 {
   nix.settings.substituters = [
     "http://hydra.v6.fyi"
-    "http://hydra-cache.v6.fyi"
   ];
   nix.settings.trusted-public-keys = [
     "hydra-cache.v6.fyi:dphYk1Lmeks4xNxCCxNT0vYaWCqBunNaqNbfEQvL/6Q="
