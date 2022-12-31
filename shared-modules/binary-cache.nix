1ac08a8fefee5b8640110445971d2210fdf7b90e
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-31 05:36:55 +0000

    Remove old hydra cache key

--- shared-modules/binary-cache.nix
+++ shared-modules/binary-cache.nix
@@ -4,7 +4,6 @@
     "http://hydra.v6.fyi"
   ];
   nix.settings.trusted-public-keys = [
-    "hydra-cache.v6.fyi:dphYk1Lmeks4xNxCCxNT0vYaWCqBunNaqNbfEQvL/6Q="
     "hydra.v6.fyi:FGmeG3K0tmxNJIFsjswRGhnHe3Apmkqcpw3CaOjqyCM="
   ];
 }
