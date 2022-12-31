5c61e14454a06b25c28bb410e2d34d10d4a8813b
Jess Schallenberg <jess.schallenberg@gmail.com>
2022-12-31 05:30:54 +0000

    Add new binary cache key

--- shared-modules/binary-cache.nix
+++ shared-modules/binary-cache.nix
@@ -5,5 +5,6 @@
   ];
   nix.settings.trusted-public-keys = [
     "hydra-cache.v6.fyi:dphYk1Lmeks4xNxCCxNT0vYaWCqBunNaqNbfEQvL/6Q="
+    "hydra.v6.fyi:FGmeG3K0tmxNJIFsjswRGhnHe3Apmkqcpw3CaOjqyCM="
   ];
 }
